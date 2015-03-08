88c94cc6ff5ab93b09414c10f25385005f1a31b8
Seung Ho Jang <shjang1992@hotmail.com>
2015-03-08 18:22:10 +0900

    Character moves and jumps Input added

---
+++ UnityProject/Assets/Script/FieldObject/PlayerCharacter.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class PlayerCharacter : MonoBehaviour
+{
+    private float speed = 1.0f;
+    private bool isJumping = false;
+    private float jumpTime = 0.0f;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        var velocity = new Vector3();
+        velocity.x = speed;
+
+        if(isJumping == true)
+        {
+            jumpTime -= Time.deltaTime;
+            if(jumpTime <= 0.0f)
+            {
+                isJumping = false;
+            }
+
+            velocity.y = 1.0f;
+        }
+
+        var dx = velocity * Time.deltaTime;
+        transform.localPosition += new Vector3(dx.x, dx.y, 0.0f);
+    }
+
+    private void Move(float speed)
+    {
+        this.speed = speed;
+    }
+
+    public void MoveLeft()
+    {
+        Move(-1);
+    }
+
+    public void MoveRight()
+    {
+        Move(1);
+    }
+
+    public void Stop()
+    {
+        Move(0);
+    }
+
+    public void Jump()
+    {
+        isJumping = true;
+        jumpTime = 1.0f;
+    }
+   
+    public float GetSpeed()
+    {
+        return speed;
+    }
+}
---
+++ UnityProject/Assets/Script/Input/Keyboard.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Keyboard : MonoBehaviour
+{
+    private void Update()
+    {
+        var pc = GetComponent<PlayerCharacter>();
+        if (pc == null)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            pc.Jump();
+        }
+
+        if (Input.GetKeyDown("left"))
+        {
+            pc.MoveLeft();
+        }
+        else if (Input.GetKeyDown("right"))
+        {
+            pc.MoveRight();
+        }
+
+        if (Input.GetKeyUp("left") || Input.GetKeyUp("right"))
+        {
+            pc.Stop();
+        }
+    }
+}
