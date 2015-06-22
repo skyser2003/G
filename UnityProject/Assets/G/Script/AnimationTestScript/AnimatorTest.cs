7eaddc81336d7e7c0ea5f43dc47d0c75d7a50f36
todd032 <todd032@naver.com>
2015-06-22 21:40:31 +0900

    CustomAnimation Added

---
+++ UnityProject/Assets/G/Script/AnimationTestScript/AnimatorTest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorTest : MonoBehaviour {
+
+	public enum AnimState
+	{
+		Idle,
+		Take_Damage,
+		Die,
+		Stunned,
+		Defend,
+		Walk,
+		Run,
+		Jump,
+		Attack01,
+		Attack02,
+
+	}
+
+	public Animator Anim;
+	public AnimState State;
+	public float StateSpeed = 1f;
+
+	public float speedtest;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Anim.speed = StateSpeed;
+			Anim.Play(State.ToString());
+		}
+
+		Anim.SetFloat("HorizontalSpeed", speedtest);
+	}
+}
