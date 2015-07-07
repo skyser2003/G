0de59b3c5cc581779d872a3cd0e0cdcf0c9ba586
Jaeyun, Oh <goodocy@gmail.com>
2015-07-07 13:13:58 +0900

    Player Struct, Class Added

---
+++ UnityProject/Assets/G/Script/Data/PlayerInfo.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[SerializeField]
+public struct PlayerInfoStruct {
+	public string name;
+	public float damage;
+	public int health;
+	public float defence;
+	public float weight;
+	public float maxMoveSpeed;
+	public float moveForce;
+	public float moveFriction;
+	public float jumpForce;
+	public int jumpNumber;
+	public float gravityFriction;
+}
+
+public class PlayerInfo : MonoBehaviour {
+
+	private PlayerInfoStruct playerData;
+
+	PlayerInfo(PlayerInfoStruct info) {
+		this.playerData = info;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
