e35e7dd62f5c87eca5862534ccd3294bf97b5b2b
Seung Ho Jang <shjang1992@hotmail.com>
2015-07-18 23:00:09 +0900

    Null pointer bug fixed

--- UnityProject/Assets/G/Script/Multiplayer/UserList.cs
+++ UnityProject/Assets/G/Script/Multiplayer/UserList.cs
@@ -10,12 +10,7 @@ class UserList
 
     }
 
-    private Dictionary<string, User> userList;
-
-    public void Init()
-    {
-        userList = new Dictionary<string, User>();
-    }
+    private Dictionary<string, User> userList = new Dictionary<string, User>();
 
     public void Add(User user)
     {
