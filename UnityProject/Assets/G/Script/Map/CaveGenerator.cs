8a6b8caea871648c270b690eb628a1a666822bc5
Seung Ho Jang <shjang1992@hotmail.com>
2015-07-23 01:48:26 +0900

    Cave generating added Has infinite loop problem for now

---
+++ UnityProject/Assets/G/Script/Map/CaveGenerator.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+enum CaveState
+{
+    Empty,
+    Wall
+}
+
+class CaveGenerator : MonoBehaviour
+{
+    private int width;
+    private int height;
+
+    private CaveState[][] map;
+
+    private void Start()
+    {
+        Generate(100, 100);
+    }
+
+    public void Generate(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+
+        map = new CaveState[width][];
+
+        for (int i = 0; i < width; ++i)
+        {
+            map[i] = new CaveState[height];
+
+            for(int j=0;j< height; ++j)
+            {
+                map[i][j] = CaveState.Wall;
+            }
+        }
+
+        Dig();
+    }
+
+    private void Dig()
+    {
+        var initialMiner = new CaveMiner();
+        initialMiner.Init(map, width / 2, height / 2);
+
+        var minerList = new List<CaveMiner>();
+        minerList.Add(initialMiner);
+
+        while (minerList.Count != 0)
+        {
+            var addList = new List<CaveMiner>();
+            var removeList = new List<CaveMiner>();
+
+            foreach (var miner in minerList)
+            {
+                bool result = miner.Dig();
+                if (result == false)
+                {
+                    removeList.Add(miner);
+                }
+            }
+
+            foreach(var miner in minerList)
+            {
+                var newMiner = miner.GenerateNew();
+                if(newMiner != null)
+                {
+                    addList.Add(newMiner);
+                }
+            }
+
+            foreach (var removeMiner in removeList)
+            {
+                minerList.Remove(removeMiner);
+            }
+
+            foreach(var newMiner in addList)
+            {
+                minerList.Add(newMiner);
+            }
+        }
+
+        for (int i = 0; i < map.Length; ++i)
+        {
+            for (int j = 0; j < map[i].Length; ++j)
+            {
+                GameObject obj = null;
+                // Empty
+                if(map[i][j] == CaveState.Empty)
+                {
+                    obj = Object.Instantiate(GameObject.Find("EmptyCave"));
+                }
+                // Wall
+                else if(map[i][j] == CaveState.Wall)
+                {
+                    obj = Object.Instantiate(GameObject.Find("WallCave"));
+                }
+
+                obj.GetComponent<Transform>().localPosition = new Vector2(i, j);
+            }
+        }
+    }
+}
---
+++ UnityProject/Assets/G/Script/Map/CaveMiner.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+
+class CaveMiner
+{
+    private CaveState[][] map;
+    private System.Random rand = new System.Random();
+    private int x, y;
+    
+    public void Init(CaveState[][] map, int x, int y)
+    {
+        this.map = map;
+        this.x = x;
+        this.y = y;
+    }
+
+    public bool Dig()
+    {
+        if (FourSidesUndiggable(x, y) == true)
+        {
+            return false;
+        }
+
+        var direction = rand.Next(4);
+        int originalX = x;
+        int originalY = y;
+
+        while (true)
+        {
+            switch (direction)
+            {
+                // Up
+                case 0:
+                    {
+                        ++y;
+                    }
+                    break;
+                // Right
+                case 1:
+                    {
+                        ++x;
+                    }
+                    break;
+                // Down
+                case 2:
+                    {
+                        --y;
+                    }
+                    break;
+                // Left
+                case 3:
+                    {
+                        --x;
+                    }
+                    break;
+            }
+
+            if (IsValid(x, y) == false)
+            {
+                x = originalX;
+                y = originalY;
+                continue;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        map[x][y] = CaveState.Empty;
+
+        return true;
+    }
+
+    public CaveMiner GenerateNew()
+    {
+        var possibility = rand.Next(100);
+        if (possibility <= 7)
+        {
+            var miner = new CaveMiner();
+            miner.Init(map, x, y);
+            return miner;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private bool FourSidesUndiggable(int x, int y)
+    {
+        if (IsValid(x - 1, y) == false || map[x - 1][y] == CaveState.Wall)
+        {
+            return false;
+        }
+        if (IsValid(x + 1, y) == false || map[x + 1][y] == CaveState.Wall)
+        {
+            return false;
+        }
+        if (IsValid(x, y - 1) == false || map[x][y - 1] == CaveState.Wall)
+        {
+            return false;
+        }
+        if (IsValid(x, y + 1) == false || map[x][y + 1] == CaveState.Wall)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool IsValid(int x, int y)
+    {
+        return 0 <= x && x < map.Length && 0 <= y && y < map[0].Length;
+    }
+}
