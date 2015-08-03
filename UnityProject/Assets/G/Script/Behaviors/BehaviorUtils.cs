cc31e96673a1116434651d6062e227353b20e450
Jaeyun, Oh <goodocy@gmail.com>
2015-08-03 18:55:28 +0900

    오브젝트들 숨기고 감추는 기능 ( Deprecated 예정 )

---
+++ UnityProject/Assets/G/Script/Behaviors/BehaviorUtils.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public static class BehaviorUtils {
+	public static void MakeVisible(GameObject obj, bool visible) {
+		Renderer renderer = obj.GetComponent<Renderer>();
+		if (renderer != null && renderer.enabled != visible) {
+			renderer.enabled = visible;
+		}
+		int childCount = obj.transform.childCount;
+		for (int i = 0; i < childCount; i++) {
+			GameObject child = obj.transform.GetChild(i).gameObject;
+			MakeVisible(child, visible);
+		}
+	}
+}
