6b808782c4dadd18f0c068129f3cdc02546a34f9
Seung Ho Jang <shjang1992@hotmail.com>
2015-08-04 01:31:57 +0900

    Begin pos and end pos exists Path must exist between begin and end

--- UnityProject/Assets/G/Script/Map/CaveGenerator.cs
+++ UnityProject/Assets/G/Script/Map/CaveGenerator.cs
@@ -12,6 +12,8 @@ class CaveGenerator : MonoBehaviour
     private int width;
     private int height;
 
+    private Vector2 begin, end;
+
     private CaveState[][] map;
 
     private void Start()
@@ -29,23 +31,38 @@ class CaveGenerator : MonoBehaviour
         for (int i = 0; i < width; ++i)
         {
             map[i] = new CaveState[height];
-
-            for (int j = 0; j < height; ++j)
-            {
-                map[i][j] = CaveState.Wall;
-            }
         }
 
         Dig();
     }
 
-    private void Dig()
+    private void InitMap()
     {
-        var initialMiner = new CaveMiner();
-        initialMiner.Init(map, width / 2, height / 2);
+        for (int i = 0; i < width; ++i)
+        {
+            for (int j = 0; j < height; ++j)
+            {
+                map[i][j] = CaveState.Wall;
+            }
+        }
+    }
 
+    private void InitMiners(List<CaveMiner> minerList)
+    {
+        var miner1 = new CaveMiner();
+        var miner2 = new CaveMiner();
+
+        miner1.Init(map, (int)begin.x, (int)begin.y);
+        miner2.Init(map, (int)end.x, (int)end.y);
+
+        minerList.Add(miner1);
+        minerList.Add(miner2);
+    }
+
+    private void GenerateMap()
+    {
         var minerList = new List<CaveMiner>();
-        minerList.Add(initialMiner);
+        InitMiners(minerList);
 
         while (minerList.Count != 0)
         {
@@ -80,42 +97,107 @@ class CaveGenerator : MonoBehaviour
                 minerList.Add(newMiner);
             }
         }
+    }
 
-        var visited = new bool[map.Length][];
-        for (int i = 0; i < map.Length; ++i)
+    private void Dig()
+    {
+        begin = new Vector2(width / 2, 0);
+        end = new Vector2(width - 1, height / 2);
+
+        while (true)
         {
-            visited[i] = new bool[map[i].Length];
-            for (int j = 0; j < map[i].Length; ++j)
-            {
-                visited[i][j] = false;
-            }
-        }
+            InitMap();
+            GenerateMap();
 
-        var chunkList = new List<List<Vector2>>();
-
-        for (int i = 0; i < map.Length; ++i)
-        {
-            for (int j = 0; j < map[i].Length; ++j)
+            var visited = new bool[map.Length][];
+            for (int i = 0; i < map.Length; ++i)
             {
-                if (map[i][j] == CaveState.Empty || visited[i][j] == true)
+                visited[i] = new bool[map[i].Length];
+                for (int j = 0; j < map[i].Length; ++j)
                 {
-                    continue;
+                    visited[i][j] = false;
+                }
+            }
+
+            // Clear filled chunks smaller than 5 in size
+            var chunkList = new List<List<Vector2>>();
+
+            for (int i = 0; i < map.Length; ++i)
+            {
+                for (int j = 0; j < map[i].Length; ++j)
+                {
+                    if (map[i][j] != CaveState.Wall || visited[i][j] == true)
+                    {
+                        continue;
+                    }
+
+                    var chunk = new List<Vector2>();
+                    GetChunk(CaveState.Wall, chunk, visited, i, j);
+                    chunkList.Add(chunk);
+                }
+            }
+
+            foreach (var chunk in chunkList)
+            {
+                if (chunk.Count <= 4)
+                {
+                    foreach (var point in chunk)
+                    {
+                        map[(int)point.x][(int)point.y] = CaveState.Empty;
+                    }
+                }
+            }
+
+            // Clear up
+            for (int i = 0; i < map.Length; ++i)
+            {
+                for (int j = 0; j < map[i].Length; ++j)
+                {
+                    visited[i][j] = false;
+                }
+            }
+
+            chunkList.Clear();
+
+            // Check if path exists from start to end
+            for (int i = 0; i < map.Length; ++i)
+            {
+                for (int j = 0; j < map[i].Length; ++j)
+                {
+                    if (map[i][j] != CaveState.Empty || visited[i][j] == true)
+                    {
+                        continue;
+                    }
+
+                    var chunk = new List<Vector2>();
+                    GetChunk(CaveState.Empty, chunk, visited, i, j);
+                    chunkList.Add(chunk);
+                }
+            }
+
+            bool pathExists = false;
+            foreach (var chunk in chunkList)
+            {
+                int counter = 2;
+                foreach (var point in chunk)
+                {
+                    if (point == begin || point == end)
+                    {
+                        --counter;
+                    }
                 }
 
-                var chunk = new List<Vector2>();
-                GetChunk(chunk, visited, i, j);
-                chunkList.Add(chunk);
-            }
-        }
-
-        foreach(var chunk in chunkList)
-        {
-            if(chunk.Count <= 4)
-            {
-                foreach(var point in chunk)
+                if (counter == 0)
                 {
-                    map[(int)point.x][(int)point.y] = CaveState.Empty;
+                    pathExists = true;
+                    break;
                 }
+            }
+
+            // Restart the process
+            if (pathExists == true)
+            {
+                break;
             }
         }
 
@@ -140,14 +222,14 @@ class CaveGenerator : MonoBehaviour
         }
     }
 
-    private void GetChunk(List<Vector2> chunk, bool[][] visited, int x, int y)
+    private void GetChunk(CaveState state, List<Vector2> chunk, bool[][] visited, int x, int y)
     {
         if ((0 <= x && x < map.Length && 0 <= y && y < map[0].Length) == false)
         {
             return;
         }
 
-        if(map[x][y] == CaveState.Empty)
+        if (map[x][y] != state)
         {
             return;
         }
@@ -156,10 +238,10 @@ class CaveGenerator : MonoBehaviour
         {
             visited[x][y] = true;
             chunk.Add(new Vector2(x, y));
-            GetChunk(chunk, visited, x - 1, y);
-            GetChunk(chunk, visited, x + 1, y);
-            GetChunk(chunk, visited, x, y - 1);
-            GetChunk(chunk, visited, x, y + 1);
+            GetChunk(state, chunk, visited, x - 1, y);
+            GetChunk(state, chunk, visited, x + 1, y);
+            GetChunk(state, chunk, visited, x, y - 1);
+            GetChunk(state, chunk, visited, x, y + 1);
         }
     }
 }
